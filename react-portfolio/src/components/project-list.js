25b10ed9dd894c0a220f5aa247d7a75ecac8994a
Zack Anderson <zack.anderson2011@gmail.com>
2021-10-06 11:08:45 -0500

    Destructured projects to disply using a projects list js file. Projects now displays on page

--- react-portfolio/src/components/project-list.js
+++ react-portfolio/src/components/project-list.js
@@ -1,55 +1,39 @@
 export const projectList = [
   {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
+    title: "Fitness Tracker",
+    subtitle: "Progressive Web App React and Mongo",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image:
-      "https://user-images.githubusercontent.com/81836426/134695267-25b5ae0c-7671-49fb-bc8f-012306766895.gif",
+      "The Fitness Tracker app allows users to create and track workouts. Users are able to: Add exercises to the most recent workout plan, add new exercises to a new workout plan on a given day. They can also view the combined weight of multiple exercises from the past seven workouts on the `stats` page.",
+    image: "./project1-gif.gif",
     link: "https://github.com/Zanderson91/fitness-tracker",
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
+    title: "Budget Tracker",
+    subtitle: "React and Mongo",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+      "The user will be able to add expenses and deposits to their budget with or without a connection. When entering transactions offline, they should populate the total when brought back online. Offline and Online Functionality allow the user full flexibility to budget wherever they are.",
+
+    image: "./BUdget-screenshot.jpg",
+    link: "https://anderson-budget-tracker.herokuapp.com/",
   },
   {
-    title: "DevChat",
-    subtitle: "React and Firebase",
+    title: "MVC Tech Blog",
+    subtitle: "MVC Paradigm using Node/Sequel/Express",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
+      "This tech blog app is a blog that allows users to create, update and delete posts. This project is unique because it authenticates users and incorporates cookies to ensure that the user experience is optimal. Handlebars are used as front end framework and communicates seamlessly with mysql to produce a quality blog site.",
+    image: "./MVC.gif",
+    link: "https://glacial-fortress-91093.herokuapp.com/dashboard",
   },
   {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
+    title: "E-Commerce Back End",
+    subtitle: "REST APIs | mySQL",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
+      "This application uses GET/POST/PUT/DELETE API routes to build a database of employees, their role and their salary. This application is a mySQL database that can be initiated and manipulated through command line. The app allows the user to effectively track and display employees by category and primary keys.",
+    image: "./employee tracker.gif",
     link: "https://reactbootcamp.com",
   },
 ];
 
-export const testimonials = [
-  {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "Jesse Hicks",
-    company: "Zoozle",
-  },
-  {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
-    name: "Ruben Alvarez",
-    company: "Dooble.io",
-  },
-];
 
 export const skills = [
   "JavaScript",
--- react-portfolio/src/components/projects.js
+++ react-portfolio/src/components/projects.js
@@ -10,8 +10,35 @@ function Projects() {
             Recent Projects
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Here are a few of my recent applications and projects I have been working on.
+            Here are a few of my recent applications and projects I have been
+            working on.
           </p>
+        </div>
+        <div className="flex flex-wrap -m-4">
+          {projectList.map((projectList) => (
+            <a
+              href={projectList.link}
+              key={projectList.image}
+              className="sm:w-1/2 w-100 p-4"
+            >
+              <div className="flex relative">
+                <img
+                  alt="gallery"
+                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  src={projectList.image}
+                />
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                    {projectList.subtitle}
+                  </h2>
+                  <h1 className="title-font text-lg font-medium text-white mb-3">
+                    {projectList.title}
+                  </h1>
+                  <p className="leading-relaxed">{projectList.description}</p>
+                </div>
+              </div>
+            </a>
+          ))}
         </div>
       </div>
     </section>
