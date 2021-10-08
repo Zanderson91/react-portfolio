f7503e4b6f80937798216d18fd2eba81c39c713c
Zack Anderson <zack.anderson2011@gmail.com>
2021-10-08 15:16:36 -0500

    updated Browser Router

--- src/App.js
+++ src/App.js
@@ -5,31 +5,33 @@ import Nav from "./components/nav";
 import Projects from "./components/projects";
 import Skills from "./components/skills";
 import Contact from "./components/contact";
-import { Route, Switch } from 'react-router';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import "./App.css";
 
 
 function App() {
   return (
-    <main className="text-gray-400 bg-gradient-to-br from-gray-900 via-gray-900 to-green-900 body-font">
-      <Nav />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <main className="text-gray-400 bg-gradient-to-br from-gray-900 via-gray-900 to-green-900 body-font">
+        <Nav />
 
-      <Switch>
-        <Route path="/" exact>
-          <Header />
-        </Route>
-        <Route path="/projects">
-          <Projects />
-        </Route>
-        <Route path="/skills">
-          <Skills />
-        </Route>
-        <Route path="/contact">
-          <Contact />
-        </Route>
-      </Switch>
-      <Footer />
-    </main>
+        <Switch>
+          <Route path="/" exact>
+            <Header />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/skills">
+            <Skills />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+        <Footer />
+      </main>
+    </BrowserRouter>
   );
 }
 
